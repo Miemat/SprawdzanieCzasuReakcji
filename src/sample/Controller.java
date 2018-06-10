1f9e69c5f9b6759f5d975c2378fa43846a1e91c1
Mateusz <mat.miernik@hotmail.com>
2018-06-10 22:06:11 +0200

    Poprawienie dzialania gry, Dodanie przycisku rozpoczecia gry

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -4,7 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
-import java.awt.event.ActionEvent;
+import java.util.Random;
 
 public class Controller {
 
@@ -14,29 +14,68 @@ public class Controller {
     @FXML
     Label time;
 
-    static Long startTime= new Long(0);
+    @FXML
+    Button resetGame;
 
-    boolean flag = true;
+
+    boolean flag = false;
+    boolean flagStartGame = false;
+
+
+    private Long startTime;
+    private Long startTimeGame;
+
 
     public void play(javafx.event.ActionEvent event){
-        System.out.println("test");
 
 
-
-        Long endTime = new Long(0);
-        Long duration = new Long(0);
-
-        if(flag){
-            this.startTime = System.nanoTime();
-            button.setStyle("-fx-background-color: lawngreen");
-            flag = false;
-        }else{
-            endTime = System.nanoTime();
-            button.setStyle("-fx-background-color: red");
-            duration = (endTime - this.startTime);
-            Long result = duration/1000000;
-            time.setText(result.toString());
-            flag = true;
+        if (flagStartGame) {
+            if (flag) {
+                endGame();
+                flagStartGame = !flagStartGame;
+            }  else if (!flag) {
+                countGame();
+            }
+        }else if(!flagStartGame){
+            playAgain();
         }
+
+    }
+
+    private void playAgain() {
+        button.setStyle("-fx-background-color: blue");
+        button.setText("Najpierw Rozpocznij gre");
+        time.setText("");
+    }
+
+    private void countGame() {
+        startTime = System.currentTimeMillis();
+        Random random = new Random();
+        final int randomTime = (random.nextInt(9000)+2);
+        Long resultRandomTime = startTime + randomTime;
+        Long timeToLoop;
+        do {
+            timeToLoop = System.currentTimeMillis();
+        } while (resultRandomTime > timeToLoop);
+        startTimeGame = System.currentTimeMillis();
+        button.setText("Klikaj !!!");
+        time.setText("");
+        button.setStyle("-fx-background-color: green");
+        flag = !flag;
+    }
+
+    private void endGame() {
+        Long endTime = System.currentTimeMillis();
+        button.setStyle("-fx-background-color: red");
+        Long resultTime = (endTime - startTimeGame);
+        button.setText("Koniec gry");
+        time.setText(resultTime.toString());
+        flag = !flag;
+    }
+
+    public void resetGame(javafx.event.ActionEvent actionEvent) {
+        button.setStyle("");
+        button.setText("Kliknij, nastepnie zareaguj \n po zmienie koloru");
+        flagStartGame = true;
     }
 }
